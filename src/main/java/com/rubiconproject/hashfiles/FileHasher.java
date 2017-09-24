43746f2aac6d675685a6867e66bb67bb91346d58
Ruslan Popenko <ruslanpopenko@ukr.net>
2017-09-24 16:57:18 +0300

    Documented FileHasher.

--- src/main/java/com/rubiconproject/hashfiles/FileHasher.java
+++ src/main/java/com/rubiconproject/hashfiles/FileHasher.java
@@ -7,23 +7,44 @@ import java.nio.charset.Charset;
 
 import static com.rubiconproject.hashfiles.StaticUtils.validateNotNull;
 
+/**
+ * Implementation of <code>Hashable</code>
+ * which takes file content and then encodes it.
+ * File content encodes with the <code></code>Sha512StringHasher</code>.
+ *
+ * @see Hashable
+ * @see Sha512StringHasher
+ */
 public class FileHasher implements Hashable {
 
     private File file;
     private Charset charset;
     private String hash;
 
+    /**
+     * Constructs FileHasher based on file which content will be encoded.
+     *
+     * @param file entire file
+     */
     public FileHasher(File file) {
         this.file = file;
         validateFile();
     }
 
+    /**
+     * Validates file for null, existing and file is normal file
+     */
     private void validateFile() {
         validateNotNull(file, "File is null");
         assert file.exists() : "File " + file.getName() + " doesn't exist";
         assert file.isFile() : file.getName() + " is not a file";
     }
 
+    /**
+     * Keeps prev calculated hash.
+     *
+     * @see Hashable#hash(Charset)
+     */
     @Override
     public String hash(final Charset charset) {
         validateNotNull(charset, "Charset is null");
@@ -43,6 +64,12 @@ public class FileHasher implements Hashable {
         return file.getName();
     }
 
+    /**
+     * Reads file content with FileInputStream as byte array
+     * and then transform it to string with certain encoding
+     * @param charset file encoding
+     * @return String representation of file content
+     */
     private String readFileContent(final Charset charset) {
         try (final FileInputStream fis = new FileInputStream(file)) {
             final byte[] data = new byte[(int) file.length()];
