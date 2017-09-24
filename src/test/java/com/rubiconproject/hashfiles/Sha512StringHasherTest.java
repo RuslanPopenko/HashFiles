4d180eb23b25c3e7afd6dec4e774c0e795d50a23
Ruslan Popenko <ruslanpopenko@ukr.net>
2017-09-24 12:17:09 +0300

    Test without guava.

--- src/test/java/com/rubiconproject/hashfiles/Sha512StringHasherTest.java
+++ src/test/java/com/rubiconproject/hashfiles/Sha512StringHasherTest.java
@@ -1,11 +1,9 @@
 package com.rubiconproject.hashfiles;
 
-import com.google.common.hash.Hashing;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -17,8 +15,7 @@ public class Sha512StringHasherTest extends AbstactHashableTest {
 
     @Before
     public void initTestData() {
-        testString = "Test string";
-        super.expectedHash = Hashing.sha512().hashString(testString, StandardCharsets.UTF_8).toString();
+        testString = "Nullam ornare, magna ac tincidunt congue, diam nisl vulputate velit, sed auctor nibh ex quis nisi. Duis suscipit purus a elementum sollicitudin. Morbi non eros vitae diam ornare auctor quis ac ipsum. Vestibulum et odio vitae mauris luctus imperdiet at quis nulla. Proin sem sapien, vestibulum sit amet vehicula vitae, sodales nec lacus. Praesent eget aliquet sem. Fusce malesuada semper luctus. Integer hendrerit neque erat, vitae dignissim justo vestibulum vel. Morbi eu purus a elit vulputate congue. Vivamus congue mattis quam et eleifend. Maecenas hendrerit elit non rutrum cursus. Vestibulum sodales arcu quis nisl ultricies maximus. Quisque turpis eros, pellentesque et nisl sit amet, elementum mollis eros. In ut massa sed lorem volutpat egestas sed elementum ex.\n";
     }
 
     @Test
@@ -43,6 +40,7 @@ public class Sha512StringHasherTest extends AbstactHashableTest {
 
     @Test
     public void hashStringTest() {
+        super.expectedHash = "af371785c4fecf30acdd648a7d4d649901eeb67536206a9f517768f0851c0a06616f724b2a194e7bc0a762636c55fc34e0fcaf32f1e852682b2b07a9d7b7a9f9";
         hashTest(new Sha512StringHasher(testString));
     }
 }
